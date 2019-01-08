1d65b9a3227e4b7e29596afececd0736c2f18383
tongmu1633 <15830691633@163.com>
2019-01-08 14:24:38 +0800

    中国文艺网视频素材存储管理系统

---
+++ src/main/java/cn/org/cflac/VideoManangerApplication.java
@@ -0,0 +1,14 @@
+package cn.org.cflac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoManangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoManangerApplication.class, args);
+	}
+
+}
+
