02bfe7686801f9ce7b57c337087b66eb88fa0330
tongmu1633 <15830691633@163.com>
2019-01-10 15:51:46 +0800

    home静态页面和部分配置文件

---
+++ src/main/java/cn/org/cflac/home/controller/HomeController.java
@@ -0,0 +1,16 @@
+package cn.org.cflac.home.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class HomeController {
+
+	@RequestMapping("/home")
+	public String hello(){
+		
+		return "home";
+		
+	}
+}
