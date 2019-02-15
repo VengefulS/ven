f511d5a0d289f72b56a4bc664dbcf15dd76f4d75
VengefulS <39764600@qq.com>
2019-02-15 14:00:49 +0800

    123456

--- src/main/java/cn/org/cflac/VideoManangerApplication.java
+++ src/main/java/cn/org/cflac/VideoManangerApplication.java
@@ -2,11 +2,19 @@ package cn.org.cflac;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class VideoManangerApplication {	
+public class VideoManangerApplication extends SpringBootServletInitializer{	
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		
+		return builder.sources(VideoManangerApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(VideoManangerApplication.class, args);
 		System.out.println("springboot 已启动！");
